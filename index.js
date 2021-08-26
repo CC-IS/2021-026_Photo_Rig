a5ed96d29080146565301fbaf74e77e12fea2b38
Yusuf Ismail <Ismaily@carleton.edu>
2021-08-26 10:20:58 -0500

    testing node gphoto2

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-console.log('hello world'
-);
+const path = require("path")
+const { app, BrowserWindow } = require('electron')
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            worldSafeExecuteJavaScript: true,
+            contextIsolation: false
+        }
+    })
+
+    win.loadFile('public/home.html')
+
+
+}
+app.whenReady().then(() => {
+    createWindow()
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
