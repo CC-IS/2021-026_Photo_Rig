38ea383d91a3ce358621d59662d12c6cbb58fa42
Yusuf Ismail <Ismaily@carleton.edu>
2021-08-26 13:27:21 -0500

    testing gphoto2 node

---
+++ public/script.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var gphoto2 = require('gphoto2');
+var GPhoto = new gphoto2.GPhoto2();
+GPhoto.setLogLevel(1);
+GPhoto.on('log', function (level, domain, message) {
+    console.log(domain, message);
+});
+
+GPhoto.list(function (list) {
+    if (list.length === 0) {
+        console.log("No cameras found");
+        return
+    };
+    var camera = list[0];
+    console.log('Found', camera.model);
+    takePicture(camera)
+});
+
+
+function takePicture(camera) {
+    camera.takePicture({ download: true }, function (er, data) {
+        fs.writeFileSync(__dirname + '/picture.jpg', data);
+    });
+}
