7ce08f111a078ff47975e2419a8658996d3db10e
Yusuf Ismail <Ismaily@carleton.edu>
2021-08-31 18:56:03 -0500

    minor tweaks in design

--- public/camera.js
+++ public/camera.js
@@ -1,17 +1,19 @@
 const { exec } = require("child_process");
 var gphoto2 = require('gphoto2');
 const { spawn } = require('child_process');
+var fs = require('fs');
 
 
 class camera {
 
     constructor() {
-        var fs = require('fs');
         this.GPhoto = new gphoto2.GPhoto2();
         this.GPhoto.setLogLevel(1);
         this.initCam()
     }
-
+    /**
+     * Initializes the camera as a device and prepares for taking pictures and setting focus etc.
+     */
     initCam() {
         this.GPhoto.list(function (list) {
             if (list.length === 0) {
@@ -22,15 +24,26 @@ class camera {
             console.log('Found', this.camera.model);
         });
     }
-
-
-    takepicture() {
+    /**
+     * Takes a pictures, names it, and saves it to the foldername specified.
+     * @param {number} position the number correspondind to the position in rig 
+     * @param {number} focus the +ve or -ve focus number from the point chosen to be the startpoint
+     * @param {String} folderName the foldername to save in in the main directory  
+     */
+    takepicture(position, focus, folderName) {
         this.camera && this.camera.takePicture({ download: true }, function (er, data) {
             er && console.error(er);
-            fs.writeFileSync(__dirname + '/picture.jpg', data);
+            let operationNumber = 0
+            // if (!fs.existsSync(__dirname + `/${folderName}`)) {
+            //     operationNumber++
+            // }
+            fs.writeFileSync(__dirname + `/${folderName}` `/${position}-${focus}.jpg`, data);
         });
     }
-
+    /**
+     * 
+     * @param {String} focus the step to give to the focus motor "Near 1", "Far 3" ... 
+     */
     setFocus(focus) {
         this.camera.setConfigValue("viewfinder", 1, function (er) {
             er && console.log(er);
@@ -39,7 +52,9 @@ class camera {
             er && console.log(er)
         })
     }
-
+    /**
+     * connects the camera as a webcam in order to view the liveview in the brower.
+     */
     connectCameraDev() {
         this.process = exec(`gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0`, (error, stdout, stderr) => {
             if (error) {
@@ -52,6 +67,9 @@ class camera {
             }
         });
     }
+    /**
+     * Connects the html element to the webcam
+     */
     displayVideo() {
         var video = document.querySelector("#videoElement");
         if (navigator.mediaDevices.getUserMedia) {
@@ -64,9 +82,13 @@ class camera {
                 });
         }
     }
+    /**
+     * Stops using the camera as webcam and initializes it as ordinary camera.
+     */
     stopVideo() {
-        const grep = spawn('grep', ['vcodec']);
-        grep.kill('SIGHUP');
+        exec("killall gphoto2", () => {
+            this.initCam()
+        })
     }
 
 }
--- public/script.js
+++ public/script.js
@@ -1,7 +1,28 @@
-const { camera } = require("./camera")
-const cam = new camera();
-var fs = require('fs');
-var gphoto2 = require('gphoto2');
-setFocus = focus => cam.setFocus(focus);
+// const { camera } = require("./camera")
+// const cam = new camera();
+// var fs = require('fs');
+// var gphoto2 = require('gphoto2');
+// setFocus = focus => cam.setFocus(focus);
 takepicture = () => cam.takePicture;
-
+let setFocus = document.querySelector("#setFocus")
+let liveview = document.querySelector("#liveView")
+let showPictures = document.querySelector("#showPictures")
+let focusSetters = document.querySelector(".focusSetters")
+setFocus.addEventListener('click', () => {
+    focusSetters.classList.toggle('hidden')
+    setFocus.classList.toggle('Active')
+    showPictures.classList.toggle('inActive')
+    liveview.classList.toggle('inActive')
+})
+showPictures.addEventListener('click', () => {
+    // Show pictures in here
+    setFocus.classList.toggle('inActive')
+    showPictures.classList.toggle('Active')
+    liveview.classList.toggle('inActive')
+})
+liveview.addEventListener('click', () => {
+    // Show liveview in here
+    setFocus.classList.toggle('inActive')
+    showPictures.classList.toggle('inActive')
+    liveview.classList.toggle('Active')
+})
