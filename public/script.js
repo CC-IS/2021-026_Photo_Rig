0af7033fc663771202714d20fc16cdf8356a1d8b
Yusuf Ismail <Ismaily@carleton.edu>
2021-09-07 15:34:20 -0500

    fixing little things

--- public/script.js
+++ public/script.js
@@ -7,25 +7,19 @@ function setFocus(focus) {
 }
 
 takepicture = () => cam.takePicture;
-let setFocus = document.querySelector("#setFocus")
+let setFocusE = document.querySelector("#setFocus")
 let liveview = document.querySelector("#liveView")
 let showPictures = document.querySelector("#showPictures")
 let focusSetters = document.querySelector(".focusSetters")
-setFocus.addEventListener('click', () => {
+setFocusE.addEventListener('click', () => {
     focusSetters.classList.toggle('hidden')
-    setFocus.classList.toggle('Active')
-    showPictures.classList.toggle('Active')
-    liveview.classList.toggle('Active')
+    // change status to enable and others to diable
 })
 showPictures.addEventListener('click', () => {
     // Show pictures in here
-    setFocus.classList.toggle('Active')
-    showPictures.classList.toggle('Active')
-    liveview.classList.toggle('Active')
+    //change status
 })
 liveview.addEventListener('click', () => {
     // Show liveview in here
-    setFocus.classList.toggle('inActive')
-    showPictures.classList.toggle('inActive')
-    liveview.classList.toggle('Active')
+    // change status
 })
